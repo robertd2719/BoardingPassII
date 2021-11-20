340c930d7daf97fb5ccd04460f4db2897d882e59
robertd2719 <45602920+robertd2719@users.noreply.github.com>
2021-11-20 13:39:01 -0500

    Initial commit

---
+++ src/mainRunner/Main.java
@@ -0,0 +1,26 @@
+package mainRunner;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = new FXMLLoader().load(getClass().getResource("mainFXML.fxml"));
+        Scene scene = new Scene(root,600,400);
+        stage.setTitle("Good Enough AirLines");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/mainRunner/MainController.java
@@ -0,0 +1,4 @@
+package mainRunner;
+
+public class MainController {
+}
